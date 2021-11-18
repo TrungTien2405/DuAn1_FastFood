7073ae26c1abc5c95544e41b8bcad8a44276a1b0
TrungTien2405 <tienntps16504@fpt.edu.vn>
2021-11-18 17:11:21 +0700

    update

--- app/src/main/java/com/example/myapplication/Fagment/QLTaiKhoanFragment.java
+++ app/src/main/java/com/example/myapplication/Fagment/QLTaiKhoanFragment.java
@@ -3,14 +3,20 @@ package com.example.myapplication.Fagment;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.viewpager2.widget.ViewPager2;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.myapplication.Adapter.ViewPagerAdapter;
 import com.example.myapplication.R;
+import com.google.android.material.tabs.TabLayout;
 
 public class QLTaiKhoanFragment extends Fragment {
+    TabLayout tabLayout;
+    ViewPager2 viewPager;
 
     public static QLTaiKhoanFragment newInstance(String param1, String param2) {
         QLTaiKhoanFragment fragment = new QLTaiKhoanFragment();
@@ -30,6 +36,39 @@ public class QLTaiKhoanFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_q_l_tai_khoan, container, false);
+        View view = inflater.inflate(R.layout.fragment_q_l_tai_khoan, container, false);
+        tabLayout = view.findViewById(R.id.tabLayout);
+        viewPager = view.findViewById(R.id.viewPager);
+
+        FragmentManager manager = getParentFragmentManager();
+        ViewPagerAdapter adapter = new ViewPagerAdapter(manager, getLifecycle());
+        viewPager.setAdapter(adapter);
+
+        tabLayout.addTab(tabLayout.newTab().setText("CHỦ NHÀ HÀNG"));
+        tabLayout.addTab(tabLayout.newTab().setText("KHÁCH HÀNG"));
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
+        viewPager.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+                tabLayout.selectTab(tabLayout.getTabAt(position));
+            }
+        });
+        return view;
     }
 }
