e1127f78dd5c48b754406ecf930af4ac4428814b
HuyLK <93662605+HuyLK@users.noreply.github.com>
2021-11-21 09:35:08 +0700

    b

--- app/src/main/java/com/example/myapplication/Fagment/CaiDatFragment.java
+++ app/src/main/java/com/example/myapplication/Fagment/CaiDatFragment.java
@@ -1,6 +1,9 @@
 package com.example.myapplication.Fagment;
 
+import static android.content.Context.MODE_PRIVATE;
+
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -8,20 +11,30 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.example.myapplication.LoginActivity;
 import com.example.myapplication.R;
+import com.example.myapplication.RegisterActivity;
+import com.google.firebase.auth.FirebaseAuth;
+
+import java.util.logging.LogManager;
 
 
 public class CaiDatFragment extends Fragment {
     TextView tvHoTen,tvSDT,tvDiaChi,tvSoDu,tvThongTinUD,tvHoTro;
+    Button btnDangXuat;
+    private FirebaseAuth auth;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        auth = FirebaseAuth.getInstance();
     }
-//    getSupportFragmentManager().beginTransaction().replace(R.id.nav_FrameFragment, new ThongTinUDFragment).commit();
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -32,7 +45,9 @@ public class CaiDatFragment extends Fragment {
         tvSoDu=v.findViewById(R.id.tv_SoDuCD);
         tvThongTinUD=v.findViewById(R.id.tv_ThongTinUngDungCD);
         tvHoTro=v.findViewById(R.id.tv_HoTroCD);
+        btnDangXuat=v.findViewById(R.id.btn_DangXuatCD);
         getDataIntent();
+
 
         tvThongTinUD.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -41,9 +56,35 @@ public class CaiDatFragment extends Fragment {
             }
         });
 
+        tvHoTro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.nav_FrameFragment, new HoTroFragment()).commit();
+            }
+        });
+
+        btnDangXuat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signOut();
+            }
+        });
+
         return v;
 
     }
+    public void signOut() {
+        SharedPreferences pref = getActivity().getSharedPreferences("USER_FILE", MODE_PRIVATE);
+        SharedPreferences.Editor edit = pref.edit();
+        edit.clear();
+        edit.commit();
+
+        auth.signOut();
+        Intent intent = new Intent(getContext(), LoginActivity.class);
+        startActivity(intent);
+
+    }
+
     private void getDataIntent(){
         Intent intent = getActivity().getIntent();
         String hoTen= intent.getStringExtra("HoTen");
